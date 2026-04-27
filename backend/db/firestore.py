748be2cfa6a5d7e1a5747a7ba5789282cd0533a4
SHASHANK-1527 <sharmashashank2112@gmail.com>
2026-04-28 02:00:21 +0530

    Changes in firestore.py for easier deployment on render

--- backend/db/firestore.py
+++ backend/db/firestore.py
@@ -1,9 +1,12 @@
+import json
+import os
+
 import firebase_admin
 from firebase_admin import credentials, firestore
 from google.cloud.firestore import Increment
-import os
 
-cred = credentials.Certificate(os.environ["FIREBASE_SERVICE_ACCOUNT_PATH"])
+service_account_info = json.loads(os.environ["FIREBASE_SERVICE_ACCOUNT_JSON"])
+cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
